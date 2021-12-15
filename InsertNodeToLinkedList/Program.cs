98dfbf6965b74611c0a67227f001adad00be4ddf
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-12-15 04:39:49 +0300

    was fixed

---
+++ InsertNodeToLinkedList/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace InsertNodeToLinkedList
+{
+    class Program
+    {
+        class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+
+            public SinglyLinkedListNode(int nodeData)
+            {
+                this.data = nodeData;
+                this.next = null;
+            }
+        }
+        class SinglyLinkedList
+        {
+            public SinglyLinkedListNode head;
+            public SinglyLinkedListNode tail;
+
+            public SinglyLinkedList()
+            {
+                this.head = null;
+                this.tail = null;
+            }
+
+            public void InsertNode(int nodeData)
+            {
+                SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+                if (this.head == null)
+                {
+                    this.head = node;
+                }
+                else
+                {
+                    this.tail.next = node;
+                }
+
+                this.tail = node;
+            }
+        }
+
+        static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep)
+        {
+            while (node != null)
+            {
+                Console.Write(node.data);
+
+                node = node.next;
+
+                if (node != null)
+                {
+                    Console.Write(sep);
+                }
+            }
+        }
+
+        static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode llist, int data, int position)
+        {
+            var current = llist;
+            SinglyLinkedListNode prev = null;
+            int iteration = 1;
+
+
+            while (current.next != null)
+            {
+                SinglyLinkedListNode node = new SinglyLinkedListNode(data);
+
+                if (position == iteration)
+                {
+                    node.next = current.next;
+                    current.next = node;
+                    return llist;
+
+
+                }
+                prev = current;
+                current = current.next;
+                iteration++;
+
+            }
+            return llist;
+
+        }
+        static void Main(string[] args)
+        {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++)
+            {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                llist.InsertNode(llistItem);
+            }
+
+            int data = Convert.ToInt32(Console.ReadLine());
+
+            int position = Convert.ToInt32(Console.ReadLine());
+
+            SinglyLinkedListNode llist_head = insertNodeAtPosition(llist.head, data, position);
+
+            PrintSinglyLinkedList(llist_head, " ");
+        }
+    }
+}
